bcba745201b7d753819b922debb7dff35e1ff96f
MarcosYH <marcosdeimos18@gmail.com>
2023-09-27 19:20:09 +0200

    add routage for pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Cours from "./pages/Cours";
+import Temoignage from "./pages/Temoignage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <div className=" ">
-       
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/course" element={<Cours />} />
+          <Route path="/testimoni" element={<Temoignage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
