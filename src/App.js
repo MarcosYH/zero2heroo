455323e79f197533381e644f071f945ff38ce59c
MarcosYH <marcosdeimos18@gmail.com>
2023-10-02 16:28:35 +0200

    feat: add routeconfigue

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import RoutesConfig from "./routes";
-import Backtotop from "./components/Backtotop";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Backtotop from "./components/client/Backtotop";
+ 
 
 function App() {
   return (
     <div className=" ">
       <Router>
         <Backtotop />
-        <Navbar />
         <RoutesConfig />
-        <Footer />
       </Router>
     </div>
   );
