a676e7d4626a00ac9540030ade8a6ea623b612e6
MarcosYH <marcosdeimos18@gmail.com>
2023-10-09 17:29:37 +0200

    feat: add table for user info

--- src/pages/admin/User.jsx
+++ src/pages/admin/User.jsx
@@ -9,77 +9,132 @@ const users = [
 ];
 
 export default function User() {
-  const [selectedUsers, setSelectedUsers] = useState([]);
-  const [isEditMode, setIsEditMode] = useState(false);
 
-  const toggleEditMode = () => {
-    setIsEditMode(!isEditMode);
-  };
-
-  const handleCheckboxChange = (userId) => {
-    const isSelected = selectedUsers.includes(userId);
-    if (isSelected) {
-      setSelectedUsers(selectedUsers.filter((id) => id !== userId));
-    } else {
-      setSelectedUsers([...selectedUsers, userId]);
-    }
-  };
   return (
     <div className='p-10 px-20 lg:ml-64'>
       <div className=' flex justify-between'>
         <h4 className=' text-3xl'> Utilisateurs</h4>
         <Link
-          to="/admin/adduser"
+          to="#!"
           className='btn btn-primary pt-3 px-3 pb-3'>
           <i class="fas fa-plus mr-3"></i>
           <span>Nouvel Utilisateur</span>
         </Link>
       </div>
-      <div>
-
-        <div className="container mx-auto p-4">
-          <button
-            className="mb-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            onClick={toggleEditMode}
-          >
-            {isEditMode ? "Terminer l'édition" : "Modifier/Supprimer"}
-          </button>
-
-          <ul className="grid grid-cols-1 gap-4">
-            {users.map((user) => (
-              <li
-                key={user.id}
-                className="bg-white shadow-md p-4 flex items-center justify-between"
-              >
-                <div className="flex items-center space-x-4">
-                  <input
-                    type="checkbox"
-                    checked={selectedUsers.includes(user.id)}
-                    onChange={() => handleCheckboxChange(user.id)}
-                    className={isEditMode ? "block" : "hidden"}
-                  />
-                  <div>
-                    <h2 className="text-xl font-bold">
-                      {user.nom} {user.prenom}
-                    </h2>
-                    <p className="text-gray-600">{user.email}</p>
-                  </div>
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-10">
+        <table className="w-full text-sm">
+          <thead className="text-md text-black uppercase bg-gray-50 ">
+            <tr>
+              <th scope="col" className="p-4">
+                <div className="flex items-center">
+                  <input id="checkbox-all-search" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                  <label htmlFor="checkbox-all-search" className="sr-only">checkbox</label>
                 </div>
-
-                {isEditMode && (
-                  <div className="space-x-2">
-                    <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
-                      Modifier
-                    </button>
-                    <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-                      Supprimer
-                    </button>
-                  </div>
-                )}
-              </li>
-            ))}
-          </ul>
-        </div>
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Nom
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Email
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Role
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Statut
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Action
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className=" border-b border-b-gray-300 hover:bg-gray-200">
+              <td className="w-4 p-4">
+                <div className="flex items-center">
+                  <input id="checkbox-table-search-1" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                  <label htmlFor="checkbox-table-search-1" className="sr-only">checkbox</label>
+                </div>
+              </td>
+              <th scope="row" className="px-6 py-4 font-medium text-black">
+                Smith Alice
+              </th>
+              <td className="px-6 py-4">
+                alice.smith@example.com
+              </td>
+              <td className="px-6 py-4">
+                Admin
+              </td>
+              <td className="px-6 py-4">
+                <span className=' bg-green-200 p-1 px-2 rounded-md text-green-700 font-medium'>Actif</span>
+              </td>
+              <td className="px-6 py-4 flex space-x-4">
+                <Link to="#" className=' text-xl text-yellow-400'>
+                  <i className='fas fa-edit'></i>
+                </Link>
+                <Link to="#" className=' text-xl text-red-500'>
+                  <i className='fas fa-trash-alt'></i>
+                </Link>
+              </td>
+            </tr>
+            <tr className=" border-b border-b-gray-300 hover:bg-gray-200">
+              <td className="w-4 p-4">
+                <div className="flex items-center">
+                  <input id="checkbox-table-search-1" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                  <label htmlFor="checkbox-table-search-1" className="sr-only">checkbox</label>
+                </div>
+              </td>
+              <th scope="row" className="px-6 py-4 font-medium text-black">
+                John Doe
+              </th>
+              <td className="px-6 py-4">
+                john.doe@example.com
+              </td>
+              <td className="px-6 py-4">
+                Enseignant
+              </td>
+              <td className="px-6 py-4">
+                <span className=' bg-green-200 p-1 px-2 rounded-md text-green-700 font-medium'>Actif</span>
+              </td>
+              <td className="px-6 py-4 flex space-x-4">
+                <Link to="#" className=' text-xl text-yellow-400'>
+                  <i className='fas fa-edit'></i>
+                </Link>
+                <Link to="#" className=' text-xl text-red-500'>
+                  <i className='fas fa-trash-alt'></i>
+                </Link>
+              </td>
+            </tr>
+            <tr className=" border-b border-b-gray-300 hover:bg-gray-200">
+              <td className="w-4 p-4">
+                <div className="flex items-center">
+                  <input id="checkbox-table-search-1" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                  <label htmlFor="checkbox-table-search-1" className="sr-only">checkbox</label>
+                </div>
+              </td>
+              <th scope="row" className="px-6 py-4 font-medium text-black">
+                Johnson Bob
+              </th>
+              <td className="px-6 py-4">
+                bob.johnson@example.com
+              </td>
+              <td className="px-6 py-4">
+                Etudiant
+              </td>
+              <td className="px-6 py-4">
+                <span className=' bg-red-200 p-1 px-2 rounded-md text-red-700 font-medium'>inactif</span>
+              </td>
+              <td className="px-6 py-4 flex space-x-4">
+                <Link to="#" className=' text-xl text-yellow-400'>
+                  <i className='fas fa-edit'></i>
+                </Link>
+                <Link to="#" className=' text-xl text-red-500'>
+                  <i className='fas fa-trash-alt'></i>
+                </Link>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   )
