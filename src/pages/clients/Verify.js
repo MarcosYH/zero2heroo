bed986733a0329037a10222bdcf2c8306beea223
MarcosYH <marcosdeimos18@gmail.com>
2023-10-27 21:16:24 +0200

    add spam detail for check_mail route

--- src/pages/clients/Verify.js
+++ src/pages/clients/Verify.js
@@ -7,6 +7,7 @@ export default function Verify() {
       <h1 className="text-3xl font-bold mb-4 text-gray-500 my-10">
         Veuillez vérifier votre boite mail pour accéder <br /> au lien de
         validation de votre inscription.
+        si vous n'avez pas reçu de mail, veuillez vérifier vos spam.
       </h1>
         </div>
       <div className=" flex justify-center items-center">
