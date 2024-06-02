2fb4765000c508b0ae8a6ff06749568b7410fc35
Marcos Y <`[marcos@wazoom-studio.com](mailto:marcos@wazoom-studio.com)`>
2024-06-02 17:36:57 +0200

    refactor : path page client

--- src/components/client/Home/ParcoursHome.js
+++ src/components/client/Home/ParcoursHome.js
@@ -2,6 +2,41 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { scrollToTop } from "../../../utils/scrollToTop";
 export default function ParcoursHome() {
+  const dataPath = [
+    {
+      id: 1,
+      image: "https://images.unsplash.com/photo-1556740758-90de374c12ad",
+      category: "Cybersécurité",
+      difficulty: "Débutant",
+      duration: "2 semaines",
+      title: "Introduction à la Cybersécurité",
+    },
+    {
+      id: 2,
+      image: "https://bit.ly/3VpMZCX",
+      category: "Cybersécurité",
+      difficulty: "Intermédiaire",
+      duration: "4 semaines",
+      title: "Analyse de Malware",
+    },
+    {
+      id: 3,
+      image: "https://images.unsplash.com/photo-1591696205602-2f950c417cb9",
+      category: "Cybersécurité",
+      difficulty: "Avancé",
+      duration: "6 semaines",
+      title: "Sécurité des Réseaux",
+    },
+    {
+      id: 4,
+      image: "https://bit.ly/3RbBQ6d",
+      category: "Cybersécurité",
+      difficulty: "Expert",
+      duration: "8 semaines",
+      title: "Hacking Éthique et Tests d'Intrusion",
+    },
+  ];
+
   return (
     <div>
       <section className="courses_section section_space_lg">
@@ -27,115 +62,53 @@ export default function ParcoursHome() {
               </div>
             </div>
           </div>
-          <div className="row">
-            <div className="col col-lg-4">
-              <div className="course_card style_2">
-                <div className="item_image">
-                  <Link to="/path" onClick={scrollToTop}>
-                    <img
-                      src="assets/images/course/course_image_4.jpg"
-                      alt="course_image_4"
-                    />
-                  </Link>
-                </div>
-                <div className="item_content">
-                  <div className="d-flex align-items-center justify-content-between mb-3">
-                    <ul className="item_category_list unordered_list">
+          <div className="row justify-content-start">
+            {dataPath.map((item, index) => (
+              <div
+                key={index}
+                className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4"
+              >
+                <div className="card h-100 course_card style_2">
+                  <div className="image-container">
+                    <Link
+                      to="/path"
+                      className="image-container"
+                      onClick={scrollToTop}
+                    >
+                      <img
+                        src={item.image}
+                        alt="parcours"
+                        className="card-img-top"
+                      />
+                    </Link>
+                  </div>
+                  <div className="card-body item_content">
+                    <div className="d-flex align-items-center justify-content-between mb-3">
+                      <ul className="list-unstyled mb-0 item_category_list unordered_list">
+                        <li>
+                          <Link to="#!">{item.category}</Link>
+                        </li>
+                      </ul>
+                    </div>
+                    <ul className="meta_info_list unordered_list justify-content-between">
                       <li>
-                        <Link to="#!">Categorie</Link>
+                        <i className="fas fa-chart-bar mr-3" />
+                        <span>{item.difficulty}</span>
+                      </li>
+                      <li>
+                        <i className="fas fa-clock" />
+                        <span>{item.duration}</span>
                       </li>
                     </ul>
+                    <h3 className="item_title">
+                      <Link to="/path" onClick={scrollToTop}>
+                        {item.title}
+                      </Link>
+                    </h3>
                   </div>
-                  <ul className="meta_info_list unordered_list justify-content-between">
-                    <li>
-                      <i className="fas fa-chart-bar mr-3" />
-                      <span>Difficulté</span>
-                    </li>
-                    <li>
-                      <i className="fas fa-clock" />
-                      <span>Durée</span>
-                    </li>
-                  </ul>
-                  <h3 className="item_title">
-                    <Link to="/path" onClick={scrollToTop}>
-                      Titre du parcours
-                    </Link>
-                  </h3>
                 </div>
               </div>
-            </div>
-            <div className="col col-lg-4">
-              <div className="course_card style_2">
-                <div className="item_image">
-                  <Link to="/path" onClick={scrollToTop}>
-                    <img
-                      src="assets/images/course/course_image_5.jpg"
-                      alt="course_image_4"
-                    />
-                  </Link>
-                </div>
-                <div className="item_content">
-                  <div className="d-flex align-items-center justify-content-between mb-3">
-                    <ul className="item_category_list unordered_list">
-                      <li>
-                        <Link to="#!">Categorie</Link>
-                      </li>
-                    </ul>
-                  </div>
-                  <ul className="meta_info_list unordered_list justify-content-between">
-                    <li>
-                      <i className="fas fa-chart-bar mr-3" />
-                      <span>Difficulté</span>
-                    </li>
-                    <li>
-                      <i className="fas fa-clock" />
-                      <span>Durée</span>
-                    </li>
-                  </ul>
-                  <h3 className="item_title">
-                    <Link to="/path" onClick={scrollToTop}>
-                      Titre du parcours
-                    </Link>
-                  </h3>
-                </div>
-              </div>
-            </div>
-            <div className="col col-lg-4">
-              <div className="course_card style_2">
-                <div className="item_image">
-                  <Link to="/path" onClick={scrollToTop}>
-                    <img
-                      src="assets/images/course/course_image_6.jpg"
-                      alt="course_image_4"
-                    />
-                  </Link>
-                </div>
-                <div className="item_content">
-                  <div className="d-flex align-items-center justify-content-between mb-3">
-                    <ul className="item_category_list unordered_list">
-                      <li>
-                        <Link to="#!">Categorie</Link>
-                      </li>
-                    </ul>
-                  </div>
-                  <ul className="meta_info_list unordered_list justify-content-between">
-                    <li>
-                      <i className="fas fa-chart-bar mr-3" />
-                      <span>Difficulté</span>
-                    </li>
-                    <li>
-                      <i className="fas fa-clock" />
-                      <span>Durée</span>
-                    </li>
-                  </ul>
-                  <h3 className="item_title">
-                    <Link to="/path" onClick={scrollToTop}>
-                      Titre du parcours
-                    </Link>
-                  </h3>
-                </div>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </section>
--- src/pages/clients/Parcours.js
+++ src/pages/clients/Parcours.js
@@ -2,6 +2,40 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Parcours() {
+  const dataParcours = [
+    {
+      id: 1,
+      image: "https://bit.ly/4bFH1Dn",
+      category: "Cybersécurité",
+      difficulty: "Débutant",
+      duration: "2 semaines",
+      title: "Introduction à la Cybersécurité",
+    },
+    {
+      id: 2,
+      image: "https://bit.ly/3VpMZCX",
+      category: "Cybersécurité",
+      difficulty: "Intermédiaire",
+      duration: "4 semaines",
+      title: "Analyse de Malware",
+    },
+    {
+      id: 3,
+      image: "https://bit.ly/3Kn0Mnv",
+      category: "Cybersécurité",
+      difficulty: "Avancé",
+      duration: "6 semaines",
+      title: "Sécurité des Réseaux",
+    },
+    {
+      id: 4,
+      image: "https://bit.ly/3RbBQ6d",
+      category: "Cybersécurité",
+      difficulty: "Expert",
+      duration: "8 semaines",
+      title: "Hacking Éthique et Tests d'Intrusion",
+    },
+  ];
   return (
     <div>
       <div className="page_wrapper">
@@ -17,11 +51,14 @@ export default function Parcours() {
                       </li>
                       <li>Parcours</li>
                     </ul>
-                    <h1 className="page_title animate__animated animate__fadeIn">Nos Parcours</h1>
+                    <h1 className="page_title animate__animated animate__fadeIn">
+                      Nos Parcours
+                    </h1>
                     <p className="page_description">
-                      Egestas sed tempus urna et pharetra. Leo integer malesuada
-                      nunc vel. Libero id faucibus nisl tincidunt eget nullam
-                      non nisi. Faucibus turpis in eu mi bibendum neque egestas
+                      Nous disposons d'une collection de Parcours de test pour
+                      vous aider à mettre à l'épreuve vos compétences en
+                      sécurité web. C'est parcours regroupent un ensenble de
+                      cours specifique en tes de vulnérabilités.
                     </p>
                   </div>
                 </div>
@@ -31,234 +68,47 @@ export default function Parcours() {
           <section className="courses_archive_section mb-5">
             <div className="container">
               <div className="row">
-                <div className="col col-lg-4">
-                  <div className="course_card style_2">
-                    <div className="item_image">
-                      <Link to="/path">
-                        <img
-                          src="assets/images/course/course_image_4.png"
-                          alt="course_image_4"
-                        />
+                {dataParcours.map((course, index) => (
+                  <div key={index} className="col col-lg-4">
+                    <div className="card h-100 course_card style_2">
+                      <Link to="/login">
+                        <div className="image-container">
+                          <img
+                            src={course.image}
+                            alt={course.title}
+                            className="card-img-top"
+                          />
+                        </div>
                       </Link>
-                    </div>
-                    <div className="item_content">
-                      <div className="d-flex align-items-center justify-content-between mb-3">
-                        <ul className="item_category_list unordered_list">
+                      <div className="card-body item_content">
+                        <div className="d-flex align-items-center justify-content-between mb-3">
+                          <ul className="item_category_list unordered_list">
+                            <li>
+                              <Link to="#!">{course.category}</Link>
+                            </li>
+                          </ul>
+                        </div>
+                        <ul className="meta_info_list unordered_list justify-content-between">
                           <li>
-                            <Link to="#!">Categorie</Link>
+                            <i className="fas fa-chart-bar mr-3" />
+                            <span>{course.difficulty}</span>
+                          </li>
+                          <li>
+                            <i className="fas fa-clock" />
+                            <span>{course.duration}</span>
+                          </li>
+                          <li>
+                            <i className="fas fa-unlock" />
+                            <span>Gratuit</span>
                           </li>
                         </ul>
+                        <h3 className="item_title">
+                          <Link to="/course">{course.title}</Link>
+                        </h3>
                       </div>
-                      <ul className="meta_info_list unordered_list justify-content-between">
-                        <li>
-                          <i className="fas fa-chart-bar mr-3" />
-                          <span>Difficulté</span>
-                        </li>
-                        <li>
-                          <i className="fas fa-clock" />
-                          <span>Durée</span>
-                        </li>
-                        <li>
-                          <i class="fas fa-unlock" />
-                          <span>Gratuit</span>
-                        </li>
-                      </ul>
-                      <h3 className="item_title">
-                        <Link to="/path">Titre du cours</Link>
-                      </h3>
                     </div>
                   </div>
-                </div>
-                <div className="col col-lg-4">
-                  <div className="course_card style_2">
-                    <div className="item_image">
-                      <Link to="/path">
-                        <img
-                          src="assets/images/course/course_image_4.png"
-                          alt="course_image_4"
-                        />
-                      </Link>
-                    </div>
-                    <div className="item_content">
-                      <div className="d-flex align-items-center justify-content-between mb-3">
-                        <ul className="item_category_list unordered_list">
-                          <li>
-                            <Link to="#!">Categorie</Link>
-                          </li>
-                        </ul>
-                      </div>
-                      <ul className="meta_info_list unordered_list justify-content-between">
-                        <li>
-                          <i className="fas fa-chart-bar mr-3" />
-                          <span>Difficulté</span>
-                        </li>
-                        <li>
-                          <i className="fas fa-clock" />
-                          <span>Durée</span>
-                        </li>
-                        <li>
-                          <i class="fas fa-lock" />
-                          <span>payant</span>
-                        </li>
-                      </ul>
-                      <h3 className="item_title">
-                        <Link to="/path">Titre du cours</Link>
-                      </h3>
-                    </div>
-                  </div>
-                </div>
-                <div className="col col-lg-4">
-                  <div className="course_card style_2">
-                    <div className="item_image">
-                      <Link to="/path">
-                        <img
-                          src="assets/images/course/course_image_4.png"
-                          alt="course_image_4"
-                        />
-                      </Link>
-                    </div>
-                    <div className="item_content">
-                      <div className="d-flex align-items-center justify-content-between mb-3">
-                        <ul className="item_category_list unordered_list">
-                          <li>
-                            <Link to="#!">Categorie</Link>
-                          </li>
-                        </ul>
-                      </div>
-                      <ul className="meta_info_list unordered_list justify-content-between">
-                        <li>
-                          <i className="fas fa-chart-bar mr-3" />
-                          <span>Difficulté</span>
-                        </li>
-                        <li>
-                          <i className="fas fa-clock" />
-                          <span>Durée</span>
-                        </li>
-                        <li>
-                          <i class="fas fa-lock" />
-                          <span>payant</span>
-                        </li>
-                      </ul>
-                      <h3 className="item_title">
-                        <Link to="/path">Titre du cours</Link>
-                      </h3>
-                    </div>
-                  </div>
-                </div>
-                <div className="col col-lg-4">
-                  <div className="course_card style_2">
-                    <div className="item_image">
-                      <Link to="/path">
-                        <img
-                          src="assets/images/course/course_image_4.png"
-                          alt="course_image_4"
-                        />
-                      </Link>
-                    </div>
-                    <div className="item_content">
-                      <div className="d-flex align-items-center justify-content-between mb-3">
-                        <ul className="item_category_list unordered_list">
-                          <li>
-                            <Link to="#!">Categorie</Link>
-                          </li>
-                        </ul>
-                      </div>
-                      <ul className="meta_info_list unordered_list justify-content-between">
-                        <li>
-                          <i className="fas fa-chart-bar mr-3" />
-                          <span>Difficulté</span>
-                        </li>
-                        <li>
-                          <i className="fas fa-clock" />
-                          <span>Durée</span>
-                        </li>
-                        <li>
-                          <i class="fas fa-lock" />
-                          <span>payant</span>
-                        </li>
-                      </ul>
-                      <h3 className="item_title">
-                        <Link to="/path">Titre du cours</Link>
-                      </h3>
-                    </div>
-                  </div>
-                </div>
-                <div className="col col-lg-4">
-                  <div className="course_card style_2">
-                    <div className="item_image">
-                      <Link to="/path">
-                        <img
-                          src="assets/images/course/course_image_4.png"
-                          alt="course_image_4"
-                        />
-                      </Link>
-                    </div>
-                    <div className="item_content">
-                      <div className="d-flex align-items-center justify-content-between mb-3">
-                        <ul className="item_category_list unordered_list">
-                          <li>
-                            <Link to="#!">Categorie</Link>
-                          </li>
-                        </ul>
-                      </div>
-                      <ul className="meta_info_list unordered_list justify-content-between">
-                        <li>
-                          <i className="fas fa-chart-bar mr-3" />
-                          <span>Difficulté</span>
-                        </li>
-                        <li>
-                          <i className="fas fa-clock" />
-                          <span>Durée</span>
-                        </li>
-                        <li>
-                          <i class="fas fa-lock" />
-                          <span>payant</span>
-                        </li>
-                      </ul>
-                      <h3 className="item_title">
-                        <Link to="/path">Titre du cours</Link>
-                      </h3>
-                    </div>
-                  </div>
-                </div>
-                <div className="col col-lg-4">
-                  <div className="course_card style_2">
-                    <div className="item_image">
-                      <Link to="/path">
-                        <img
-                          src="assets/images/course/course_image_4.png"
-                          alt="course_image_4"
-                        />
-                      </Link>
-                    </div>
-                    <div className="item_content">
-                      <div className="d-flex align-items-center justify-content-between mb-3">
-                        <ul className="item_category_list unordered_list">
-                          <li>
-                            <Link to="#!">Categorie</Link>
-                          </li>
-                        </ul>
-                      </div>
-                      <ul className="meta_info_list unordered_list justify-content-between">
-                        <li>
-                          <i className="fas fa-chart-bar mr-3" />
-                          <span>Difficulté</span>
-                        </li>
-                        <li>
-                          <i className="fas fa-clock" />
-                          <span>Durée</span>
-                        </li>
-                        <li>
-                          <i class="fas fa-lock" />
-                          <span>payant</span>
-                        </li>
-                      </ul>
-                      <h3 className="item_title">
-                        <Link to="/path">Titre du cours</Link>
-                      </h3>
-                    </div>
-                  </div>
-                </div>
+                ))}
               </div>
             </div>
           </section>
